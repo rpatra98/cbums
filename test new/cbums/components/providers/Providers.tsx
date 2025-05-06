74bceffeb16d04845011b399460c69efe482cf60
NuclearTeddy22 <nuclearteddy98@gmail.com>
2025-05-06 18:05:30 +0530

    Fix React dependency issues: Update React to 19.1.0 and replace react-query with @tanstack/react-query

--- test new/cbums/components/providers/Providers.tsx
+++ test new/cbums/components/providers/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from "next-auth/react";
 import React, { useState } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import ThemeProvider from "./ThemeProvider";
 
